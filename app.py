91143e0e47a25ee3f88c48c7c64fef58d2f494ca
Demezhan Marikov <demezhan1998@gmail.com>
2019-07-26 03:31:24 +0600

    db was added.

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
+db = SQLAlchemy(app)
+
+
+class Auth(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String(200))
+
+    def __repr__(self):
+        return self.token
 
 
 @app.route('/')
