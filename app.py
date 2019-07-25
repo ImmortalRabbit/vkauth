9e8317c880164ba0150612ab35031e22f50ebf05
Demezhan Marikov <demezhan1998@gmail.com>
2019-07-26 04:39:56 +0600

    Debug.

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
     if request.method == 'POST':
         access_code_url = 'https://oauth.vk.com/authorize?client_id=' + vk_id \
                           + '&display=page&redirect_uri=' + vk_url\
-                          + '&scope=friends&response_type=code'
+                          + '&scope=friends&response_type=token'
         return redirect(access_code_url)
     else:
         return render_template('index.html')
@@ -32,14 +32,11 @@ def index():
 
 @app.route('/profile', methods=['POST', 'GET'])
 def profile():
-    access_code = request.args.get('code')
-    print(access_code)
-    access_token_url = 'https://oauth.vk.com/authorize?client_id=' + vk_id \
-                       + '&display=page&redirect_uri=' + vk_url \
-                       + '&client_secret=' + vk_secret + '&code=' + access_code
-    token = requests.get(access_token_url).content
-    return token
-    # return render_template('profile.html')
+    access_token = request.args.get('access_token')
+    user_id = request.args.get('user_id')
+    print('User id ' + user_id)
+    print('Access token' + access_token)
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
