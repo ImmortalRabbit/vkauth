6b31f663001d28cc1a98a8320924fad76ba6cd62
Demezhan Marikov <demezhan1998@gmail.com>
2019-07-26 03:55:47 +0600

    VK API getting access code.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
 db = SQLAlchemy(app)
+
+vk_id = '7069242'
+vk_secret = '0HGWyQwWskHkoyR9f2dz'
+vk_url = 'https://vkauth-web.herokuapp.com/'
 
 
 class Auth(db.Model):
@@ -14,9 +18,17 @@ class Auth(db.Model):
         return self.token
 
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        access_code_url = 'https://oauth.vk.com/authorize?client_id='+ vk_id \
+                          + '&display=page&redirect_uri='+ vk_url\
+                          + '&scope=friends&response_type=token&v=5.52'
+        return redirect(access_code_url)
+
+
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
